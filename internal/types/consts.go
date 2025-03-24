3bb71c43ffb4cce6da445d01b993d48462117c10
Diego B. Arruda <62727921+Ddieguin@users.noreply.github.com>
2025-03-24 17:31:31 -0300

    chore: url consts

--- internal/types/consts.go
+++ internal/types/consts.go
@@ -1,15 +1,15 @@
 package types
 
 const (
-	GETSPEEDPROFILE    = "smartolt.com/api/system/get_speed_profiles"
-	GETONUDETAILS      = "smartolt.com/api/onu/get_onu_details"
-	GETONUSIGNAL       = "smartolt.com/api/onu/get_onu_signal"
-	UPDATESPEEDPROFILE = "smartolt.com/api/onu/update_onu_speed_profiles"
-	REBOOTONU          = "smartolt.com/api/onu/reboot"
-	DISABLEONU         = "smartolt.com/api/onu/disable/"
-	ENABLEONU          = "smartolt.com/api/onu/enable"
-	STATUSESONUS       = "smartolt.com/api/onu/get_onu_statuses?olt_id=1" //temos apenas uma OLT...
+	GETSPEEDPROFILE    = "/system/get_speed_profiles"
+	GETONUDETAILS      = "/onu/get_onu_details"
+	GETONUSIGNAL       = "/onu/get_onu_signal"
+	UPDATESPEEDPROFILE = "/onu/update_onu_speed_profiles"
+	REBOOTONU          = "/onu/reboot"
+	DISABLEONU         = "/onu/disable/"
+	ENABLEONU          = "/onu/enable"
+	STATUSESONUS       = "/onu/get_onu_statuses?olt_id=1" //temos apenas uma OLT...
 
 	DefaultAPIKey  = ""
-	DefaultBaseURL = "https://enx."
+	DefaultBaseURL = "https://enx.smartolt.com/api"
 )
